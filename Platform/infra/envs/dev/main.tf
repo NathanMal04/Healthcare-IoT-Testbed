1e18ed78ead57e491a8b30c62e3b4ff9d9a610b5
Ipule Pipi <ipipi2022@fit.edu>
2026-08-23 12:27:26 -0400

    Preserve API Gateway error responses
    Declare the response_templates that AWS already applies by default on
    the DEFAULT_4XX/DEFAULT_5XX gateway responses, so the Step 2 apply
    does not silently clear them.

--- Platform/infra/envs/dev/main.tf
+++ Platform/infra/envs/dev/main.tf
@@ -686,6 +686,10 @@ resource "aws_api_gateway_gateway_response" "default_4xx" {
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "'https://vzoniq.com'"
   }
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
 }
 
 resource "aws_api_gateway_gateway_response" "default_5xx" {
@@ -694,6 +698,10 @@ resource "aws_api_gateway_gateway_response" "default_5xx" {
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "'https://vzoniq.com'"
+  }
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
   }
 }
 
